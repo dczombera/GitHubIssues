a2e6344c79f3056baadd4710fb9de91a61e1161a
Dennis Czombera <dennis@cringle.net>
2017-01-08 17:48:21 +0100

    * Issues.CLI: Update module to use “Issues.TableFormatter.print_table_for_columns/2” in order to print a table with parsed issue data fetched from Github.

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -1,4 +1,5 @@
 defmodule Issues.CLI do
+  import Issues.TableFormatter, only: [ print_table_for_columns: 2 ]
   @default_count 4
 
   @moduledoc """
@@ -45,6 +46,7 @@ defmodule Issues.CLI do
     |> decode_response
     |> sort_into_ascending_order
     |> Enum.take(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
   end
 
   def sort_into_ascending_order(list_of_issues) do
